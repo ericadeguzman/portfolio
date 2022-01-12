a9eecd4711c3da298faaf27eb8b75934659f7f15
erricadg <was_erica@yahoo.com>
2022-01-11 20:39:49 -0800

    stuff

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { makeStyles } from "@material-ui/styles";
 import { useTheme } from "@material-ui/core/styles";
 import { ExternalLink } from "react-external-link";
@@ -12,9 +12,13 @@ import Divider from "@material-ui/core/Divider";
 import { useMediaQuery } from "@material-ui/core";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
+import emailjs from "@emailjs/browser";
 
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 
+const Result = () => {
+  return <p>Your message has been sent. talk to you soon!</p>;
+};
 const useStyles = makeStyles((theme) => ({
   container: {
     marginRight: "auto",
@@ -22,7 +26,7 @@ const useStyles = makeStyles((theme) => ({
     marginTop: 50,
     marginBottom: 50,
     [theme.breakpoints.down("xs")]: {
-      maxWidth: "350px",   
+      maxWidth: "350px",
     },
   },
   button: {
@@ -37,6 +41,30 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function Contact(props) {
+  const [result, showResult] = useState(false);
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_yqmd7nv",
+        "template_7bee1g8",
+        form.current,
+        "user_dSKPK8gx0rnh8sppymvI5"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+    showResult(true);
+  };
+
   const classes = useStyles();
 
   const [name, setName] = useState("");
@@ -71,169 +99,107 @@ export default function Contact(props) {
   return (
     <React.Fragment>
       <Box my={5}>
-        <Grid container direction="column" alignContent="center" className={classes.container}>
-          <Grid item>
-            <Box my={2}>
-              <Typography variant="h4">Hello!</Typography>
-            </Box>
-          </Grid>
-          <Grid item>
-            <Divider />
-            <Box my={2}>
-              <Typography variant="body1">
-                Ready to a start a project with me?
-              </Typography>
-              <Box mt={1}>
-                <Typography variant="body1">
-                  Email me at
-                  <a
-                    style={{
-                      textDecoration: "none",
-                      fontWeight: "700",
-                      color: "#525268",
-                    }}
-                    href="mailto:erricadg@gmail.com"
-                  >
-                    {" "}
-                    erricadg@gmail.com
-                  </a>{" "}
-                  or fill out the form below:
-                </Typography>
-              </Box>
-            </Box>
-          </Grid>
-          <Grid item>
-            <Box my={2}>
-              <TextField
-                fullWidth
-                label="Name"
-                id="name"
-                value={name}
-                onChangeCapture={(event) => setName(event.target.value)}
-              />
-            </Box>
-          </Grid>
-          <Grid item>
-            <Box my={2}>
-              <TextField
-                fullWidth
-                label="Email"
-                helperText={emailHelper}
-                error={emailHelper.length !== 0}
-                id="email"
-                value={email}
-                onChange={onChange}
-              />
-            </Box>
-          </Grid>
-          <Grid item>
-            <Box my={2}>
-              <TextField
-                InputProps={{ disableUnderline: true }}
-                value={message}
-                className={classes.message}
-                multiline
-                rows={10}
-                fullWidth
-                id="message"
-                onChangeCapture={(event) => setMessage(event.target.value)}
-              />
-            </Box>
-          </Grid>
+        <form ref={form} onSubmit={sendEmail}>
           <Grid
             container
-            item
-            direction="row"
-            xs={10}
-            md={5}
-            justifyContent="flex-end"
+            direction="column"
+            alignContent="center"
+            className={classes.container}
           >
-            <Box my={1}>
-              <Button
-                disabled={
-                  name.length === 0 ||
-                  message.length === 0 ||
-                  emailHelper.length !== 0
-                }
-                color="primary"
-                className={classes.button}
-                variant="contained"
-                onClick={() => setOpen(true)}
-              >
-                Send Message
-              </Button>
-            </Box>
+            <Grid item>
+              <Box my={2}>
+                <Typography variant="h4">Hello!</Typography>
+              </Box>
+            </Grid>
+            <Grid item>
+              <Divider />
+              <Box my={2}>
+                <Typography variant="body1">
+                  Ready to a start a project with me?
+                </Typography>
+                <Box mt={1}>
+                  <Typography variant="body1">
+                    Email me at <a style={{textDecoration: "none", fontWeight: "700", color: "#525268",}} href="mailto:erricadg@gmail.com">erricadg@gmail.com </a> 
+                    or fill out the form below:
+                  </Typography>
+                </Box>
+              </Box>
+            </Grid>
+
+            <Grid item>
+              <Box my={2}>
+                <TextField
+                  name="name"
+                  fullWidth
+                  label="Name"
+                  id="name"
+                  value={name}
+                  onChangeCapture={(event) => setName(event.target.value)}
+                />
+              </Box>
+            </Grid>
+            <Grid item>
+              <Box my={2}>
+                <TextField
+                  name="email"
+                  fullWidth
+                  label="Email"
+                  helperText={emailHelper}
+                  error={emailHelper.length !== 0}
+                  id="email"
+                  value={email}
+                  onChange={onChange}
+                />
+              </Box>
+            </Grid>
+            <Grid item>
+              <Box my={2}>
+                <TextField
+                  InputProps={{ disableUnderline: true }}
+                  value={message}
+                  className={classes.message}
+                  name="message"
+                  multiline
+                  rows={10}
+                  fullWidth
+                  id="message"
+                  onChangeCapture={(event) => setMessage(event.target.value)}
+                />
+              </Box>
+            </Grid>
+            <Grid
+              container
+              item
+              direction="row"
+              xs={12}
+              md={5}
+              justifyContent="flex-end"
+            >
+              <Box my={1}>
+                <Button
+                  disabled={
+                    name.length === 0 ||
+                    message.length === 0 ||
+                    emailHelper.length !== 0
+                  }
+                  color="primary"
+                  className={classes.button}
+                  variant="contained"
+                  type="submit"
+                  value="Send"
+                >
+                  Send Message 
+                  <MailOutlineIcon  style={{paddingLeft: '10px'}}/>
+                </Button>   
+              </Box>
+            </Grid>
+            <Grid item container  direction="row" xs={12} md={5}  justifyContent="flex-end">
+            <Typography variant="body1">
+                {result ? <Result /> : null}
+                </Typography>
+            </Grid>
           </Grid>
-          <Dialog  style={{zIndex: '1302',}} open={open} onCliose={() => setOpen(false)} PaperProps={{style:{ padding: '2em', width: '360px'} }}>
-              <DialogContent>
-              <Grid item>
-                <Typography textAlign="center" variant="h5" gutterBottom>Confirm Message</Typography>
-              </Grid>
-         
-                <Grid item>
-                  <Box my={2}>
-                    <TextField
-                      fullWidth
-                      label="Name"
-                      id="name"
-                      value={name}
-                      onChangeCapture={(event) => setName(event.target.value)}
-                    />
-                  </Box>
-                </Grid>
-                <Grid item>
-                  <Box my={2}>
-                    <TextField
-                      fullWidth
-                      label="Email"
-                      helperText={emailHelper}
-                      error={emailHelper.length !== 0}
-                      id="email"
-                      value={email}
-                      onChange={onChange}
-                    />
-                  </Box>
-                </Grid>
-                <Grid item>
-                    <TextField
-                      InputProps={{ disableUnderline: true }}
-                      value={message}
-                      className={classes.message}
-                      multiline
-                      rows={10}
-                      fullWidth
-                      id="message"
-                      onChangeCapture={(event) =>
-                        setMessage(event.target.value)
-                      }
-                    />
-                </Grid>
-           <Grid item container style={{marginTop:'1em',}}  justifyContent="flex-end"
-  alignItems="center">
-             <Grid item>
-             <Button onClick={() => setOpen(false)} color="primary"  style={{marginRight: '10px',}}>
-               Cancel
-             </Button>
-             </Grid>
-             <Grid item>
-             <Button 
-              disabled={
-                name.length === 0 ||
-                message.length === 0 ||
-                emailHelper.length !== 0
-              }
-              color="primary"
-              className={classes.button}
-              variant="contained"
-              onClick={() => setOpen(true)}
-              >
-               Send Message
-             </Button>
-             </Grid>
-           </Grid>
-              </DialogContent>
-          </Dialog>
-        </Grid>
+        </form>
       </Box>
     </React.Fragment>
   );
